9db2b39246298ec7d93741e150161a9a9310509f
Guilherme Pires <guilhermeemanuel45@gmail.com>
2021-01-16 15:00:11 -0300

    Create page selection category

---
+++ cria_app/src/pages/SelectionCategory/cardItem.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { CardImage, CardLabel, CardItens, Styles } from "./styles";
+
+const categoryImg = require('../../../assets/1400x900.png');
+
+
+interface Category {
+    id: number;
+    name: string;
+}
+
+type Props = {
+    category: Category;
+    onSelectCategory: (category: Category) => void;
+    isSelected: boolean;
+};
+
+export default function CardItem({ category, onSelectCategory, isSelected }: Props) {
+    return (
+
+        <CardItens onPress={() => onSelectCategory(category)} isSelected>
+            <CardLabel>{category.name}</CardLabel>
+            <CardImage source={categoryImg} style={[isSelected ? Styles.active : Styles.inactive]} />
+        </CardItens>
+
+    );
+}
--- cria_app/src/pages/SelectionCategory/styles.ts
+++ cria_app/src/pages/SelectionCategory/styles.ts
@@ -1,10 +1,93 @@
 import React from "react";
 import styled from "styled-components/native";
+import { StyleSheet } from "react-native";
+
+
+export const Styles = StyleSheet.create({
+  active: {
+    opacity: 0.6,
+  },
+  inactive: {
+    backgroundColor: '#7e8389',
+    // borderWidth: 2,
+    // borderRadius: 10,
+  }
+});
 
 export const Container = styled.View`
   flex: 1;
   background-color: #ffffff;
+  padding: 20px;
+`;
+export const HeaderArea = styled.View`
+  top: 30px;
+  flex-direction: row;
+  height: 50px;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  width: 250px;
+  font-size: 22px;
+  color: #7e8389;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  padding-right: 10px;
+  height: 50px;
+  color: #7e8389;
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  top : 50px;
+`;
+
+export const ViewConfirmeButton = styled.View`
+  bottom: 40px;
   text-align: center;
   align-items: center;
   justify-content: center;
+`;
+
+export const ConfirmeButton = styled.TouchableOpacity`
+  width: 300px;
+  height: 60px;
+  border-radius: 10px;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  background-color: #fd5555;
+`;
+
+export const ConfirmeText = styled.Text`
+  font-weight: bold;
+  color: #ffffff;
+  font-size: 20px;
+`;
+
+export const CardBody = styled.View`
+  flex: 1;
+  flex-direction : row;
+  flex-wrap : wrap;
+  justify-content : space-between;
+`;
+
+export const CardItens = styled.TouchableOpacity`
+  align-items : center;
+`;
+
+export const CardLabel = styled.Text`
+  color: #7e8389;
+  margin-bottom: 2px;
+  margin-top: 8px;
+  font-size : 20px;
+  font-weight : bold;
+`;
+
+export const CardImage = styled.Image`
+  height : 200px;
+  width: 150px;
+  border-width: 2px;
+  border-radius: 10px;
 `;
