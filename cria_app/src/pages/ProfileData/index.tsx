80b9bbdcdcf66b3c25fad3be6c1a46dd82987393
douglas-cezaro <cezarodouglas@gmail.com>
2021-01-15 23:26:16 -0300

    Stack navigation configuration

---
+++ cria_app/src/pages/ProfileData/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Container } from "../ProfileData/styles";
+
+export default function CheckCode() {
+  return (
+    <Container>
+      <Text style={{ textAlign: "center", alignItems: "center" }}>
+        Page ProfileData
+      </Text>
+    </Container>
+  );
+}
